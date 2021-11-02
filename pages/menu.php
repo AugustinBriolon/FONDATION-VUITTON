416ee3dea4c1a154c1188ac09b482104eee2331e
Augustin <augustin.briolon@gmail.com>
2021-11-02 16:54:59 +0100

    communaction between pages

--- pages/menu.php
+++ pages/menu.php
@@ -10,10 +10,10 @@
 			<img src="../img/croix.svg" class="icone-fermer" alt="Icone Croix"/>
 			<li><a href="../pages/index.php" class="menu__item"><?php echo $_SESSION["user"];?></a></li>
 			<li><a href="../pages/index.php" class="menu__item">Home</a></li>
-			<li><a href="../pages/pages/histoire.php" class="menu__item">History</a></li>
-			<li><a href="../pages/pages/galerie.php" class="menu__item">Gallery</a></li>
-			<li><a href="../pages/pages/blog.php" class="menu__item">Blog</a></li>
-			<li><a href="../pages/pages/contact.php" class="menu__item">Contact</a></li>
+			<li><a href="../pages/histoire.php" class="menu__item">History</a></li>
+			<li><a href="../pages/galerie.php" class="menu__item">Gallery</a></li>
+			<li><a href="../pages/blog.php" class="menu__item">Blog</a></li>
+			<li><a href="../pages/contact.php" class="menu__item">Contact</a></li>
 
 			<?php
 				if (isset($_SESSION['role'])){
